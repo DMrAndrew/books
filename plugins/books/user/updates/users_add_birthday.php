357234f2a2dff49b02f12eeb66a10e1f1879f787
Andrew <dolzhenko.andrew@gmail.com>
2022-11-02 21:43:48 +0300

    https://youtrack.mobecan.com/agiles/100-104/101-420?issue=VK-4

---
+++ plugins/books/user/updates/users_add_birthday.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Books\User\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class UsersAddBirthday extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->date('birthday')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('birthday');;
+        });
+    }
+}
