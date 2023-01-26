7d4d5725a525e87c12fa0503655f2d3a02b31398
andrew <dolzhenko.andrew@gmail.com>
2023-01-26 06:01:56 +0300

    small

--- themes/demo/assets/js/modal.js
+++ themes/demo/assets/js/modal.js
@@ -3,19 +3,15 @@ function openModal(container, buttonOpenModal) {
     const buttonOpen = $(buttonOpenModal);
     const buttonClose = $(`${container} [data-modal="close"]`);
 
-    console.log('openModal')
     if (!buttonOpen || !buttonOpen || !buttonClose) {
         return
     }
-    console.log('openModal bind')
     buttonOpen.click(() => {
-        console.log('buttonOpen click')
         modal.addClass(`active`);
         // document.body.style.overflow = 'hidden';
     })
 
     modal.click(() => {
-        console.log('modal click')
         let t = $(this);
         if (t.hasClass(`overlay`)) {
             modal.removeClass(`active`);
