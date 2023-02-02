167d3af4c6f496ca84a766bdce3d9e970eacfa16
andrew <dolzhenko.andrew@gmail.com>
2023-02-02 15:30:26 +0300

    VK-23 Читалка - Изменение размера шрифта и цвета фона с сохранением состояния.

--- themes/demo/assets/js/reader.js
+++ themes/demo/assets/js/reader.js
@@ -1,13 +1,59 @@
-let restrictCopy = function () {
-    document.ondragstart = noselect;
-// запрет на перетаскивание
-    document.onselectstart = noselect;
-// запрет на выделение элементов страницы
-    document.oncontextmenu = noselect;
-
-// запрет на выведение контекстного меню
-    function noselect() {
-        return false;
+function initReader() {
+    window.reader = {
+        reader: document.querySelector('.reader'),
+        readerBody: document.querySelector('.reader__body'),
+        fzDcrBtn: document.querySelector('.reader-decrease-fz'),
+        fzEncrcBtn: document.querySelector('.reader-increase-fz'),
+        bgDcrBtn: document.querySelector('.reader-decrease-bg'),
+        bgEncrBtn: document.querySelector('.reader-increase-bg'),
+        curBgIndex: parseInt(Cookies.get('reader_background_color_index')) || 0,
+        bgClasses: ['_bg-black', '_bg-gray', '_bg-yellow', '_bg-white'],
+        maxFont: 24,
+        curFont: parseInt(Cookies.get('reader_font_size')) || 16,
+        minFont: 12,
+        decreaseBG: function () {
+            this.curBgIndex = this.curBgIndex - 1 < 0 ? this.curBgIndex : this.curBgIndex - 1;
+            this.changeBg();
+        },
+        increaseBG: function () {
+            this.curBgIndex = this.curBgIndex + 1 > this.bgClasses.length - 1 ? this.curBgIndex : this.curBgIndex + 1;
+            this.changeBg();
+        },
+        changeBg: function () {
+            for (var i = 0; i < this.bgClasses.length; i++) {
+                this.reader.classList.toggle(this.bgClasses[i], i == this.curBgIndex);
+            }
+            this.setCookie()
+            this.changeBtnsState();
+        },
+        decreaseFZ: function () {
+            this.curFont = this.curFont - 2 < this.minFont ? this.curFont : this.curFont - 2;
+            this.setFontSize();
+        },
+        increaseFZ: function () {
+            this.curFont = this.curFont + 2 > this.maxFont ? this.curFont : this.curFont + 2;
+            this.setFontSize();
+        },
+        setFontSize: function () {
+            this.readerBody.style.fontSize = `${this.curFont}px`;
+            this.setCookie()
+            this.changeBtnsState();
+        },
+        changeBtnsState: function () {
+            this.bgDcrBtn.classList.toggle('_disabled', this.curBgIndex <= 0);
+            this.bgEncrBtn.classList.toggle('_disabled', this.curBgIndex >= this.bgClasses.length - 1);
+            this.fzDcrBtn.classList.toggle('_disabled', this.curFont <= this.minFont);
+            this.fzEncrcBtn.classList.toggle('_disabled', this.curFont >= this.maxFont);
+        },
+        setCookie() {
+            Cookies.set(`reader_font_size`, this.curFont, {expires: 2000})
+            Cookies.set(`reader_background_color_index`, this.curBgIndex, {expires: 2000})
+        },
+        init: function () {
+            this.setFontSize()
+            this.changeBg()
+        }
     }
-
+    window.reader.init();
 }
+
---
+++ themes/demo/assets/js/restrictCopy.js
@@ -0,0 +1,13 @@
+let restrictCopy = function () {
+    document.ondragstart = noselect;
+// запрет на перетаскивание
+    document.onselectstart = noselect;
+// запрет на выделение элементов страницы
+    document.oncontextmenu = noselect;
+
+// запрет на выведение контекстного меню
+    function noselect() {
+        return false;
+    }
+
+}
