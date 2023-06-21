2b91ffd830d770b8cadd2f05e42aad623ebb8e98
andrew <dolzhenko.andrew@gmail.com>
2023-06-22 01:15:28 +0300

    VK-25 Fix  - fix Rater.php  - fix migrations

--- plugins/books/book/updates/add_trackers_ip.php
+++ plugins/books/book/updates/add_trackers_ip.php
@@ -2,6 +2,7 @@
 
 namespace Books\Book\Updates;
 
+use Books\Book\Models\Tracker;
 use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 use Schema;
@@ -10,13 +11,23 @@ class AddTrackersIp extends Migration
 {
     public function up(): void
     {
-        Schema::table('books_book_trackers', function (Blueprint $table) {
+        Schema::table($this->table(), function (Blueprint $table) {
+            if (!Schema::hasColumn($table->getTable(), 'ip')) {
+                $table->string('ip');
+            }
             $table->unsignedBigInteger('user_id')->nullable()->change();
-            $table->string('ip');
         });
     }
 
-    public function down()
+    public function down(): void
     {
+        if (Schema::hasColumn($this->table(), 'ip')) {
+            Schema::dropColumns($this->table(), 'ip');
+        }
+    }
+
+    protected function table(): string
+    {
+        return (new Tracker())->getTable();
     }
 }
