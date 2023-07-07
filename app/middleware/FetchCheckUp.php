5f7d684c28d81accf944d75baf5a23cac06571b8
andrew <dolzhenko.andrew@gmail.com>
2023-07-07 13:29:13 +0300

    small

--- app/middleware/FetchCheckUp.php
+++ app/middleware/FetchCheckUp.php
@@ -13,7 +13,7 @@ class FetchCheckUp
     public function handle($request, $next)
     {
         $response = $next($request);
-        LastModified::set(Carbon::parse('2023-07-07 00:00'));
+        LastModified::set(Carbon::parse('2023-07-07 13:29'));
         if (Auth::getUser()?->fetchRequired()) {
             return $response->withCookie(Cookie::make(CookieEnum::FETCH_REQUIRED->value, 1, httpOnly: false));
         }
