0edb089d7d693769e28e6b50a70e8dc8d5f9a877
andrew <dolzhenko.andrew@gmail.com>
2023-08-04 03:30:38 +0300

    small ref

--- plugins/books/blog/components/BlogPost.php
+++ plugins/books/blog/components/BlogPost.php
@@ -32,7 +32,8 @@ class BlogPost extends ComponentBase
     public function init()
     {
         $this->post = Post
-            ::where('slug', $this->param('post_slug'))
+            ::query()
+            ->slug($this->param('post_slug'))
             ->published()
             ->firstOrFail();
 
