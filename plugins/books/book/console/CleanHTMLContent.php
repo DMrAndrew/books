2990b1fdebd3d1326409fab254dec1977145df2d
Elion Alisov <blaqdog@mail.ru>
2023-09-20 17:23:47 +0300

    minor update

--- plugins/books/book/console/CleanHTMLContent.php
+++ plugins/books/book/console/CleanHTMLContent.php
@@ -7,67 +7,32 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 /**
- * DeleteNotActivatedFreePromocodes Command
+ * CleanHTMLContent Command
  *
  * @link https://docs.octobercms.com/3.x/extend/console-commands.html
  */
-class DeleteNotActivatedFreePromocodes extends Command
+class CleanHTMLContent extends Command
 {
     /**
      * @var string signature for the console command.
      */
-    protected $signature = 'book:promocodes:delete_free_promocodes_not_activated';
+    protected $signature = 'book:content:clean_html
+                            {--type= : type of record}
+                            {--id= : id(s) of records}';
 
     /**
      * @var string description is the console command description
      */
-    protected $description = 'Удаление неиспользованных промокодов, сгенерированных в безлимитный период, по истечении безлимитного периода';
+    protected $description = 'Чистка html контента. Удаление лишних тегов, аттрибутов, стилей, --type = объект (book_content, book_annotation, blog_post, author_profile), --id = список ID записей (через запятую)';
 
     /**
      * handle executes the console command.
      */
     public function handle()
     {
-        $this->output->writeln("Удаление неиспользованных промокодов");
+        $this->output->writeln("Очистка html контента");
 
-        /**
-         * Безлимитный период заканчивается - дата
-         */
-        $unlimitedPeriodExpiredAt = Carbon::now()
-            ->subMonths(PromocodeGenerationLimiter::UNLIMITED_GENERATION_FREE_MONTHS)
-            ->endOfDay();
-
-        /**
-         * Промокоды, которые сгенерированы в первые N месяцев регистрации профиля
-         */
-        $expiredFreePromocodes = Promocode
-            ::with('profile')
-
-            /**
-             * Которые еще не использовались. Использованные не удаляем
-             */
-            ->notActivated()
-
-            /**
-             * Созданы в безлимитный период
-             */
-            ->where('created_at', '<', $unlimitedPeriodExpiredAt)
-
-            /**
-             * Профиль создан тоже в безлимитный период. Иначе профиль может быть давно существующим
-             */
-            ->whereHas('profile', function ($profile) use ($unlimitedPeriodExpiredAt) {
-                $profile->where('created_at', '<', $unlimitedPeriodExpiredAt);
-            })
-            ->get();
-
-        $expiredPromocodesCount = $expiredFreePromocodes->count();
-
-        $expiredFreePromocodes->each(function ($promocode) {
-            $promocode->delete();
-        });
-
-        $this->output->writeln("Удалено {$expiredPromocodesCount} промокодов");
+        //todo
 
         return;
     }
