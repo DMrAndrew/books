2c5a67db56eff06ddf8fc4e57a3afb45a14fcf24
Андрей <dolzhenko.andrew@gmail.com>
2023-09-26 18:28:44 +0300

    Исправлен scope owner для модели автора

--- plugins/books/book/models/Author.php
+++ plugins/books/book/models/Author.php
@@ -15,6 +15,7 @@ use October\Rain\Database\Traits\Validation;
  *
  * @method BelongsTo book
  * @method BelongsTo profile
+ *
  * @property  Profile profile
  */
 class Author extends Model
@@ -62,7 +63,7 @@ class Author extends Model
 
     public function scopeOwner(Builder $builder, $value = true): Builder
     {
-        return $builder->when(is_bool($value), fn($b) => $b->where('is_owner', '=', $value));
+        return $builder->when(is_bool($value), fn ($b) => $b->where('is_owner', '=', $value), fn ($b) => $b->where('is_owner', '=', true));
     }
 
     public function scopeCoAuthors(Builder $builder): Builder
