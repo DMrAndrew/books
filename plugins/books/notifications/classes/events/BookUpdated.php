4557572ee663ac8805099624739670e5f4069005
Elion Alisov <blaqdog@mail.ru>
2023-09-29 17:22:59 +0300

    notification bookUpdated update

--- plugins/books/notifications/classes/events/BookUpdated.php
+++ plugins/books/notifications/classes/events/BookUpdated.php
@@ -36,7 +36,6 @@ class BookUpdated extends BaseEvent
     public static function makeParamsFromEvent(array $args, $eventName = null): array
     {
         $book = Arr::get($args, 0);
-        $author = $book->authors()->owner(true)->first();
 
         $symbolsCount = (int) Arr::get($args, 1);
 
@@ -44,7 +43,6 @@ class BookUpdated extends BaseEvent
             static::defaultParams(),
             [
                 'book' => Arr::get($args, 0),
-                'author' => $author,
                 'symbols_count' => $symbolsCount,
                 'recipients' => static::getRecipients($args),
             ],
