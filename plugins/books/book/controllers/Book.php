63e76db91ff47bc88b8c4fbc5f002ec3be1a9963
Elion Alisov <blaqdog@mail.ru>
2023-10-04 09:21:04 +0300

    profiles username in users list

--- plugins/books/book/controllers/Book.php
+++ plugins/books/book/controllers/Book.php
@@ -7,6 +7,9 @@ use Backend\Behaviors\ListController;
 use Backend\Behaviors\RelationController;
 use Backend\Classes\Controller;
 use BackendMenu;
+use Books\Book\Models\Author;
+use Books\Book\Models\Book as BookModel;
+use Books\Profile\Models\Profile;
 
 /**
  * Book Backend Controller
@@ -46,5 +49,20 @@ class Book extends Controller
         parent::__construct();
 
         BackendMenu::setContext('Books.Book', 'book', 'book');
+    }
+
+    public function listExtendQuery($query)
+    {
+        /**
+         * Search by user profile
+         */
+        $bookTableName = (new BookModel())->getTable();
+        $authorsTableName = (new Author)->getTable();
+        $profileTableName = (new Profile())->getTable();
+
+        $query->addSelect($profileTableName . '.username as profile_name');
+
+        $query->leftJoin($authorsTableName, $authorsTableName . '.book_id', '=', $bookTableName . '.id');
+        $query->leftJoin($profileTableName, $authorsTableName . '.profile_id', '=', $profileTableName . '.id');
     }
 }
