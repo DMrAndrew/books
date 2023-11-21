de75611ffc3676c91478b0a7299da6de22bb8b62
Timur Moiseenko <angrav6@gmail.com>
2023-11-21 15:44:59 +0500

    created plugin AuthorProgram, DiscontService

---
+++ plugins/books/authorprograms/updates/create_authors_programs_table.php
@@ -0,0 +1,37 @@
+<?php namespace Books\AuthorPrograms\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+/**
+ * CreateAuthorsProgramsTable Migration
+ *
+ * @link https://docs.octobercms.com/3.x/extend/database/structure.html
+ */
+return new class extends Migration
+{
+    /**
+     * up builds the migration
+     */
+    public function up()
+    {
+        Schema::create('books_authors_programs', function(Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('program', 100);
+            $table->jsonb('condition');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * down reverses the migration
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books_authorprograms_authors_programs');
+    }
+};
