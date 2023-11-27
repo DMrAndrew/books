8dadac27251dde4b1ef49ba97c2c790a830d9d12
Timur Moiseenko <angrav6@gmail.com>
2023-11-27 12:29:38 +0500

    added commands for notify authors who have a bithday

---
+++ plugins/books/authorprograms/console/AuthorBirthdayNotificationCommand.php
@@ -0,0 +1,42 @@
+<?php namespace Books\AuthorPrograms\Console;
+
+use Books\Profile\Models\Profile;
+use Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use RainLab\User\Models\User;
+
+/**
+ * AuythorBeforeBirsdayNotificationCommand Command
+ *
+ * @link https://docs.octobercms.com/3.x/extend/console-commands.html
+ */
+class AuthorBirthdayNotificationCommand extends Command
+{
+    /**
+     * @var string signature for the console command.
+     */
+    protected $signature = 'authorprograms:birthday_notification';
+
+    /**
+     * @var string description is the console command description
+     */
+    protected $description = 'Оповещение автора за 3 дня до ДР что у него не будет коммисии';
+
+    /**
+     * handle executes the console command.
+     */
+    public function handle()
+    {
+        $object = \Books\Book\Models\SystemMessage::where('name', 'День рождения автора')->first();
+
+        $users = User::leftJoin('books_profile_profiles as profile', 'users.id', '=', 'profile.id')
+            ->leftJoin('books_book_authors as author', 'author.profile_id', '=', 'profile.id')
+            ->whereMonth('users.birthday', Carbon::now()->format('m'))
+            ->whereDay('users.birthday', Carbon::now()->format('d'))
+            ->distinct()
+            ->get('users.id');
+
+        Event::fire('system::message', [$object, $users]);
+    }
+}
