5b68724a8e7700dac1263c3271bd9f05d3bb4274
Андрей <dolzhenko.andrew@gmail.com>
2024-01-14 22:12:12 +0300

    fix

--- plugins/books/book/classes/converters/PDF.php
+++ plugins/books/book/classes/converters/PDF.php
@@ -13,6 +13,7 @@ class PDF extends BaseConverter
 
     public function generate(): string
     {
+        ini_set('pcre.backtrack_limit', '5000000');
         $html = Twig::parse(file_get_contents(plugins_path('/books/book/views/pdf_template.htm')), [
             'title' => $this->book->title,
             'authors' => $this->book->profiles->pluck('username')->join(', '),
@@ -23,11 +24,19 @@ class PDF extends BaseConverter
         ]);
         $pdf = new Mpdf(['tempDir' => storage_path('/temp/electronic_books_generate')]);
         $pdf->SetTitle($this->title());
+        $pdf->useSubstitutions = false;
+        $pdf->simpleTables = true;
         $pdf->SetAuthor($this->book->profile->username);
         $pdf->SetKeywords($this->book->tags->pluck('name')->join(', '));
         $pdf->SetCreator(env('APP_URL'));
+
         $pdf->WriteHTML($html);
 
         return $pdf->Output(null, Destination::STRING_RETURN);
+    }
+
+    public function __destruct()
+    {
+        ini_set('pcre.backtrack_limit', '1000000');
     }
 }
