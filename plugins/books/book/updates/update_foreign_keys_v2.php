218b2c9adeec8aaf1f8ac7873a6b79cc97bc9084
Elion Alisov <blaqdog@mail.ru>
2024-01-30 15:52:52 +0300

    migration add foreign key author references to book

---
+++ plugins/books/book/updates/update_foreign_keys_v2.php
@@ -0,0 +1,100 @@
+<?php namespace Books\Profile\Updates;
+
+use Db;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+use Schema;
+
+/**
+ * UpdateForeignKeys Migration
+ *
+ * @link https://docs.octobercms.com/3.x/extend/database/structure.html
+ */
+return new class extends Migration
+{
+    /**
+     * up builds the migration
+     */
+    public function up()
+    {
+        /**
+         * #############################
+         * Authors to books
+         * #############################
+         */
+        echo 'Authors table' . PHP_EOL;
+        $originTable = 'books_book_authors';
+        $originKey = 'book_id';
+        $foreignTable = 'books_book_books';
+        $foreignKey = 'id';
+
+        $this->deleteNotExistedRecords($originTable, $originKey, $foreignTable, $foreignKey);
+
+        Schema::table("{$originTable}", function (Blueprint $table) use ($foreignTable, $foreignKey, $originKey) {
+            $table->unsignedBigInteger("{$originKey}")->change();
+        });
+
+        $this->addForeignKey($originTable, $originKey, $foreignTable, $foreignKey);
+    }
+
+    /**
+     * down reverses the migration
+     */
+    public function down()
+    {
+        $foreigns = [
+            ['books_book_authors', 'book_id'],
+        ];
+        foreach ($foreigns as $foreign) {
+            [$originTable, $originKey] = $foreign;
+            Schema::table($originTable, function (Blueprint $table) use ($originKey) {
+                $table->dropForeign([$originKey]);
+            });
+        }
+    }
+
+    /**
+     * Delete rows for non-exist foreign records
+     *
+     * @param $originTable
+     * @param $originKey
+     * @param $foreignTable
+     * @param $foreignKey
+     *
+     * @return void
+     */
+    public function deleteNotExistedRecords($originTable, $originKey, $foreignTable, $foreignKey): void
+    {
+        $rowsDeleted = DB::table("{$originTable}")
+            ->whereNotIn("{$originTable}.{$originKey}", function ($query) use ($foreignTable, $foreignKey) {
+                $query->select("{$foreignKey}")
+                    ->from("{$foreignTable}")
+                    ->whereNotNull("{$foreignKey}");
+            })
+            ->delete();
+
+        if ($rowsDeleted > 0) {
+            echo "Удалено {$rowsDeleted} записей со ссылкой на несуществующие записи" . PHP_EOL;
+        }
+    }
+
+    /**
+     * Add foreign key
+     *
+     * @param $originTable
+     * @param $originKey
+     * @param $foreignTable
+     * @param $foreignKey
+     *
+     * @return void
+     */
+    public function addForeignKey($originTable, $originKey, $foreignTable, $foreignKey): void
+    {
+        Schema::table("{$originTable}", function (Blueprint $table) use ($foreignTable, $foreignKey, $originKey) {
+            $table->foreign("{$originKey}")
+                ->references("{$foreignKey}")
+                ->on("{$foreignTable}")
+                ->onDelete('cascade');
+        });
+    }
+};
