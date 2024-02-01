0237467472f38ebaae179bd09a497d4b81cfd15c
Андрей <dolzhenko.andrew@gmail.com>
2024-02-01 17:06:02 +0300

    optimize

--- config/horizon.php
+++ config/horizon.php
@@ -225,7 +225,7 @@ return [
             'maxJobs' => 10,
             'memory' => 1024,
             'tries' => 1,
-            'timeout' => 10000,
+            'timeout' => 1200,
             'nice' => 0,
             'balanceMaxShift' => 2,
             'balanceCooldown' => 2,
