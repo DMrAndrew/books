007cbab3597f9165f357819f0c5ceb764a321478
Андрей <dolzhenko.andrew@gmail.com>
2024-02-01 13:51:13 +0300

    fix clear trackers

--- plugins/books/book/jobs/ClearTrackers.php
+++ plugins/books/book/jobs/ClearTrackers.php
@@ -65,7 +65,7 @@ class ClearTrackers implements ShouldQueue
         $total_deleted = 0;
         $total_processed = 0;
         $processed = $this->processed();
-        $total = $this->trackerQuery()->get()->filter(fn ($i) => ! in_array($i->id, $processed))->count();
+        $total = $this->trackerQuery()->count();
         unset($processed);
         $this->notify(sprintf('%s found', $total));
         foreach ($this->trackerQuery()->cursor() as $item) {
